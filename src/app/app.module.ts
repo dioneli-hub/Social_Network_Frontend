cb30de3fdc4ddd38b89ed832d2d0bd8147734f0f
Diana Levchenko <dianka_levchenko@inbox.ru>
2023-05-10 15:36:19 +0200

    Main page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PostComponent } from './post/post.component';
+import { CommentComponent } from './comment/comment.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { SmallUserIconComponent } from './small-user-icon/small-user-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    PostComponent,
+    CommentComponent,
+    UserInfoComponent,
+    SmallUserIconComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/small-user-icon/small-user-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-user-icon',
+  templateUrl: './small-user-icon.component.html',
+  styleUrls: ['./small-user-icon.component.css']
+})
+export class SmallUserIconComponent {
+
+}
