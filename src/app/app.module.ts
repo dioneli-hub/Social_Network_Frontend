24d91bd15650511ebac8a624220fabb77adc7c2c
Diana Levchenko <dianka_levchenko@inbox.ru>
2023-06-20 18:01:37 +0200

    Secure Image pipe added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RegistrationComponent } from './registration/registration.component';
 import {AuthInterceptor} from "../interceptors/auth.interceptor";
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { SecureImagePipe } from 'src/security/secureImage.pipe';
 
 const AUTH_INTERCEPTOR = {
   provide: HTTP_INTERCEPTORS,
@@ -38,7 +39,8 @@ const routes: Routes = [
     UserInfoComponent,
     SmallUserIconComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    SecureImagePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/security/secureImage.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {ApplicationFileModel} from "../api-models/user.model";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import {Observable} from "rxjs";
+
+@Pipe({
+  name: 'secureImage'
+})
+export class SecureImagePipe implements PipeTransform{
+  constructor(private httpClient: HttpClient) {
+  }
+
+  async transform(file: ApplicationFileModel | null): Promise<string> {
+    console.log(file)
+    if (!file || !this.isImage(file)) {
+      return new Promise(resolve => {
+        resolve('/assets/person.jpg');
+      });
+    }
+
+    const imageBlob = await this.getSecureFile(file).toPromise();
+    const reader = new FileReader();
+    return new Promise(resolve => {
+      reader.onloadend = () => resolve(reader.result as string);
+      reader.readAsDataURL(imageBlob);
+    });
+  }
+
+  isImage(file: ApplicationFileModel): boolean {
+    return file.contentType.startsWith('image/');
+  }
+
+  getSecureFile(file: ApplicationFileModel): Observable<any> {
+    return this.httpClient.get(`${environment.apiUrl}file/${file.id}`, {responseType: 'blob'});
+  }
+}
